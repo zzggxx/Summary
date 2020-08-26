644e0a166e327a263c48dbb4b2cde75867a7cf87
Will <zhougaoxiongcall@163.com>
2020-08-26 20:04:19 +0800

    backup

--- c_project/temp/main.c
+++ c_project/temp/main.c
@@ -3,45 +3,10 @@
 
 #include "fun.h"
 
-const
 
-int main01()
+int main()
 {
-
-    unsigned int a = 10;
-	if ( 9- a < 0){
-		printf("结果小于0\n");
-	}
-	else{
-		printf("结果大于0\n");
-	}
-
-//test();
-
-//large_file_copy();
-
-//large_file_copy();
-
-//file_struct_read();
-//file_struct_write();
-
-//    enum_example();
-
-    //method_fget();
-
-//    method_fprintf();
-//    method_fscanf();
-
-//      method_fwrite();
-//      method_fread();
-
-//struct_example();
-
-
-
-
-
-
+    system("calc");
 }
 
 
